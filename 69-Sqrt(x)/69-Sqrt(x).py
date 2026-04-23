0b4460ab0cb93ab577e2fac2a3469e4e5054b631
RENATO G SANTOS <135625956+Renato-G-Santos@users.noreply.github.com>
2026-04-23 11:45:30 -0300

    Create 69-Sqrt(x)

---
+++ 69-Sqrt(x)/69-Sqrt(x).py
@@ -0,0 +1,9 @@
+# Last updated: 23/04/2026, 11:45:31
+1class Solution(object):
+2    def mySqrt(self, x):
+3        """
+4        :type x: int
+5        :rtype: int
+6        """
+7        
+8        return int((x ** 0.5))
