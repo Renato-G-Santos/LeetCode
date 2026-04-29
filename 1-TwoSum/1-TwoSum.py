b9d6571f4d1847ab7189b608549d017833a92706
RENATO G SANTOS <135625956+Renato-G-Santos@users.noreply.github.com>
2026-04-29 19:09:53 -0300

    File updated at 29/04/2026, 19:10:04

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,12 +1,13 @@
-# Last updated: 20/04/2026, 19:23:59
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if (nums[i] + nums[j]) == target and i != j:
-                    return i, j
+# Last updated: 29/04/2026, 19:10:04
+1class Solution(object):
+2    def twoSum(self, nums, target):
+3        """
+4        :type nums: List[int]
+5        :type target: int
+6        :rtype: List[int]
+7        """
+8        hmap = {}
+9        for idx, vl in enumerate(nums):
+10            if hmap.get(vl) is not None:
+11                return [hmap.get(vl), idx]
+12            hmap[target-vl] = idx
